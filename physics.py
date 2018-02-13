2755508bc0f82cc252eebaa125b4c135fb11d175
Excel High <longhuy322000@gmail.com>
2018-02-12 20:35:54 -0800

    ignore

--- physics.py
+++ physics.py
@@ -1,5 +1,5 @@
 from pyfrc.physics import drivetrains
-
+import math
 
 class PhysicsEngine(object):
     '''
@@ -15,6 +15,9 @@ class PhysicsEngine(object):
 
         self.physics_controller = physics_controller
         self.physics_controller.add_device_gyro_channel('adxrs450_spi_0_angle')
+        self.left_distance = 0
+        self.right_distance = 0
+        self.DIAMETER_WHEEL = 0.4375
 
     def update_sim(self, hal_data, now, tm_diff):
         '''
@@ -32,7 +35,16 @@ class PhysicsEngine(object):
         lf_motor = hal_data['pwm'][2]['value']*-1
         rf_motor = hal_data['pwm'][0]['value']*-1
 
-        speed, rotation = drivetrains.four_motor_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor)
-        if abs(speed) > 0:
-            rotation -= 0.3
+        speed, rotation, leftSpeed, rightSpeed = drivetrains.four_motor_drivetrain(lr_motor, rr_motor, lf_motor, rf_motor)
+        #if abs(speed) > 0:
+        #    rotation -= 0.3
         self.physics_controller.drive(speed, rotation, tm_diff)
+        self.left_distance += abs(leftSpeed * tm_diff)
+        self.right_distance += abs(rightSpeed * tm_diff)
+        self.left_counter = self.left_distance / (self.DIAMETER_WHEEL * math.pi)
+        self.right_counter = self.right_distance / (self.DIAMETER_WHEEL * math.pi)
+
+        hal_data['encoder'][0]['counter'] = self.left_counter
+        hal_data['encoder'][1]['counter'] = self.right_counter
+
+        print(leftSpeed, rightSpeed)
